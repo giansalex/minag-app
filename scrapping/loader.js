dda5c1fde9923d016155ac7bdcd94e4af9b6f6d2
Giancarlos Salas <giansalex@gmail.com>
2018-03-13 18:23:21 -0500

    add loaders + test

--- scrapping/loader.js
+++ scrapping/loader.js
@@ -1,6 +1,13 @@
 var axios = require('axios');
 const URL_MINAG = 'http://sistemas.minag.gob.pe/sisap/portal2/mayorista/';
+const URL_MERCADO = 'http://sistemas.minag.gob.pe/sisap/portal2/mayorista/generos/filtrarPorMercado';
 
-exports.getPage = function () {
+exports.getIndexPage = function () {
     return axios.get(URL_MINAG);
+};
+
+exports.getProductsPage = function (mercado) {
+    var params = 'mercado=' + mercado +'&&ajax=true';
+
+    return axios.post(URL_MERCADO, params);
 };
--- tests/scrapping/load.test.js
+++ tests/scrapping/load.test.js
@@ -2,9 +2,27 @@ var assert = require('assert');
 var loader = require('./../../scrapping/loader');
 
 describe('WebPage', function() {
-
+    this.timeout(10000);
     it('carga pagina de principal de minag', function() {
-        return loader.getPage()
+        return loader.getIndexPage()
+            .then(function (response) {
+                const data = response.data;
+                assert.ok(data);
+                console.log(data);
+            });
+    });
+
+    it('carga productos de todos los mercados', function () {
+        return loader.getProductsPage('*')
+            .then(function (response) {
+                const data = response.data;
+                assert.ok(data);
+                console.log(data);
+            });
+    });
+
+    it('carga productos de Mcdo mayorista cereales, leguminosas y oleaginosas', function () {
+        return loader.getProductsPage('15011506')
             .then(function (response) {
                 const data = response.data;
                 assert.ok(data);
