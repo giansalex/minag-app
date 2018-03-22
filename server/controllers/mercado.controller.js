876e6adeb3eb057c06de8291f28a23830b03efc0
Giancarlos Salas <giansalex@gmail.com>
2018-03-22 15:12:36 -0500

    add list product endpoint by market

--- server/controllers/mercado.controller.js
+++ server/controllers/mercado.controller.js
@@ -1,5 +1,6 @@
 var mercado = require('./../scrapping/mercado');
 var loader = require('./../scrapping/loader');
+var product = require('./../scrapping/product');
 var cherio = require('cheerio');
 
 exports.index = function (req, res, next) {
@@ -9,5 +10,16 @@ exports.index = function (req, res, next) {
 
             var mercados = mercado.getVariables(html);
             res.json(mercados);
+        });
+};
+
+exports.products = function (req, res, next) {
+    const code = req.params.code;
+    loader.getProductsByMercadoPage(code)
+        .then(function (response) {
+            const data = response.data;
+            var products = product.list(cherio.load(data));
+
+            res.json(products);
         });
 };
--- server/routes/mercado.js
+++ server/routes/mercado.js
@@ -3,4 +3,5 @@ var ctrl = require('../controllers/mercado.controller');
 module.exports = function (router) {
     /* GET mercados. */
     router.get('/api/mercado', ctrl.index);
+    router.get('/api/mercado/:code/products', ctrl.products);
 };
